461b0bbdd01e52945113561e0df7cb0b151bd334
Ramesh9100 <chitrarameshgowd@gmail.com>
2023-11-21 12:10:18 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Ramesh(request):
+    return render(request,'Ramesh.html')
+
+def Ramudu(request):
+    return render(request,'Ramudu.html')
+
+def Kiran(request):
+    return render(request,'Kiran.html')
